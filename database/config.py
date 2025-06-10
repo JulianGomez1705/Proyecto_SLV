9c66cf8754292d3cbbd4ceedc51e4e3a9b029f0f
Julian <jcgomez@ucatolica.edu.co>
2025-06-09 19:37:11 -0500

    pequeña correcion en la database

--- database/config.py
+++ database/config.py
@@ -1,20 +1,22 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
+# ¡Tu "External Database URL" de Render PostgreSQL pegada aquí!
+SQLALCHEMY_DATABASE_URL = "postgresql://proyecto_superliga_db_user:XqMuKEs4rkn1cqTWNcHGdGCzDWYbHAzc@dpg-d13nmcodl3ps7391vv6g-a.oregon-postgres.render.com/proyecto_superliga_db"
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    SQLALCHEMY_DATABASE_URL,
+    pool_pre_ping=True
 )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-
-# 'Base' ya NO se define aquí. Se definirá en database/models.py
+Base = declarative_base()
 
 def obtener_sesion_bd():
-    db = SessionLocal()
+    sesion_bd = SessionLocal()
     try:
-        yield db
+        yield sesion_bd
     finally:
-        db.close()
+        sesion_bd.close()
